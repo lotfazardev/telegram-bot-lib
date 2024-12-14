2eaa507a02f228799edbda1e54ea787aa393fd49
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-14 13:40:37 +0330

    docs: added jsdoc for InlineKeyboardMarkup

--- docs/TODO.md
+++ docs/TODO.md
@@ -89,7 +89,7 @@ Latest version : 🚀 December 4, 2024 ([Bot API 8.1](https://core.telegram.org/
   - [x] KeyboardButtonRequestChat [🔗](https://core.telegram.org/bots/api#keyboardbuttonrequestchat)
   - [x] KeyboardButtonPollType [🔗](https://core.telegram.org/bots/api#keyboardbuttonpolltype)
   - [x] ReplyKeyboardRemove [🔗](https://core.telegram.org/bots/api#replykeyboardremove)
-  - [ ] InlineKeyboardMarkup [🔗](https://core.telegram.org/bots/api#inlinekeyboardmarkup)
+  - [x] InlineKeyboardMarkup [🔗](https://core.telegram.org/bots/api#inlinekeyboardmarkup)
   - [ ] InlineKeyboardButton [🔗](https://core.telegram.org/bots/api#inlinekeyboardbutton)
   - [ ] LoginUrl [🔗](https://core.telegram.org/bots/api#loginurl)
   - [ ] SwitchInlineQueryChosenChat [🔗](https://core.telegram.org/bots/api#switchinlinequerychosenchat)
--- src/@types/InlineKeyboardMarkup.ts
+++ src/@types/InlineKeyboardMarkup.ts
@@ -1,5 +1,23 @@
 import { InlineKeyboardButton } from './InlineKeyboardButton'
 
+/**
+ * Represents an inline keyboard that appears right next to the message it belongs to.
+ * Inline keyboards are used for interactive buttons that appear directly below the message, allowing users to
+ * interact without sending additional messages.
+ *
+ */
 export interface InlineKeyboardMarkup {
+  /**
+   * An array of button rows, each represented by an array of {@link InlineKeyboardButton} objects.
+   * Each row contains inline buttons that can trigger actions like sending callback queries or opening URLs.
+   *
+   * @example
+   * [
+   *   [
+   *     { text: "Button 1", callback_data: "action_1" },
+   *     { text: "Button 2", callback_data: "action_2" }
+   *   ]
+   * ]
+   */
   inline_keyboard: InlineKeyboardButton[][]
 }
