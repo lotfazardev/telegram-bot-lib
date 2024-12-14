3b8ca46b64a23daab44a25e1cf4e7632b7d12082
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-14 13:14:27 +0330

    docs: added jsdoc for BusinessOpeningHours

--- src/@types/BusinessOpeningHoursInterval.ts
+++ src/@types/BusinessOpeningHoursInterval.ts
@@ -1,4 +1,14 @@
+/**
+ * Represents a time interval of business opening hours.
+ */
 export interface BusinessOpeningHoursInterval {
+  /**
+   * Opening time in minutes from midnight (00:00).
+   */
   opening_minute: number
+
+  /**
+   * Closing time in minutes from midnight (00:00).
+   */
   closing_minute: number
 }
