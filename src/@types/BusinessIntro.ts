3c787faa143ab1dcf917fdfaa1f0e42cacd6c1c9
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-14 13:23:49 +0330

    docs: added jsdoc for BusinessLocation & BusinessIntro

--- src/@types/BusinessIntro.ts
+++ src/@types/BusinessIntro.ts
@@ -1,7 +1,22 @@
 import { Sticker } from './Sticker'
 
+/**
+ * Contains information about the start page settings of a Telegram Business account.
+ */
 export interface BusinessIntro {
+  /**
+   * Optional. Title text of the business intro.
+   */
   title?: string
+
+  /**
+   * Optional. Message text of the business intro.
+   */
   message?: string
+
+  /**
+   * Optional. Sticker of the business intro.
+   * See {@link Sticker} for more details.
+   */
   sticker?: Sticker
 }
