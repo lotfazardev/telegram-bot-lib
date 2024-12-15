2464586a4f7c053df6831cd4fa5a9bbce869a7f5
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-15 17:41:42 +0330

    docs: added jsdoc for ChatBoost

--- docs/TODO.md
+++ docs/TODO.md
@@ -144,7 +144,7 @@ Latest version : 🚀 December 4, 2024 ([Bot API 8.1](https://core.telegram.org/
   - [ ] ChatBoostSourcePremium [🔗](https://core.telegram.org/bots/api#chatboostsourcepremium)
   - [ ] ChatBoostSourceGiftCode [🔗](https://core.telegram.org/bots/api#chatboostsourcegiftcode)
   - [ ] ChatBoostSourceGiveaway [🔗](https://core.telegram.org/bots/api#chatboostsourcegiveaway)
-  - [ ] ChatBoost [🔗](https://core.telegram.org/bots/api#chatboost)
+  - [x] ChatBoost [🔗](https://core.telegram.org/bots/api#chatboost)
   - [x] ChatBoostUpdated [🔗](https://core.telegram.org/bots/api#chatboostupdated)
   - [x] ChatBoostRemoved [🔗](https://core.telegram.org/bots/api#chatboostremoved)
   - [ ] UserChatBoosts [🔗](https://core.telegram.org/bots/api#userchatboosts)
--- src/entity/@types/ChatBoost.ts
+++ src/entity/@types/ChatBoost.ts
@@ -1,8 +1,36 @@
 import { ChatBoostSource } from './ChatBoostSource'
 
+/**
+ * Represents information about a chat boost.
+ *
+ * A chat boost is a temporary promotion to increase the visibility or engagement of a chat.
+ * This object contains details about the boost such as the unique identifier, timestamps, and
+ * the source of the boost.
+ *
+ * @see {@link ChatBoostSource} - The source of the chat boost (e.g., premium subscription, gift code).
+ */
 export interface ChatBoost {
+  /**
+   * The unique identifier of the boost.
+   */
   boost_id: string
+
+  /**
+   * The timestamp (Unix time) when the chat was boosted.
+   * Represents the exact moment the boost was applied.
+   */
   add_date: number
+
+  /**
+   * The timestamp (Unix time) when the boost will automatically expire.
+   * The boost expires unless the booster’s Telegram Premium subscription is extended.
+   */
   expiration_date: number
+
+  /**
+   * The source of the added boost.
+   *
+   * @see {@link ChatBoostSource} for details about how the boost was obtained.
+   */
   source: ChatBoostSource
 }
