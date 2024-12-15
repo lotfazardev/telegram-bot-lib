35eb4e63ad8a08ce772347c8cd550f2b39bd99d2
Mohammad H. Lotfazar <developer.lotfazar@gmail.com>
2024-12-15 15:10:05 +0330

    feat: add jsdocs for EncryptedPassportElementType

---
+++ src/entity/@types/EncryptedPassportElementType.ts
@@ -0,0 +1,18 @@
+/**
+ * type field in the EncryptedPassportElement interface.
+ * Represents all possible Telegram Passport element types.
+ */
+export type EncryptedPassportElementType =
+  | 'personal_details'
+  | 'passport'
+  | 'driver_license'
+  | 'identity_card'
+  | 'internal_passport'
+  | 'address'
+  | 'utility_bill'
+  | 'bank_statement'
+  | 'rental_agreement'
+  | 'passport_registration'
+  | 'temporary_registration'
+  | 'phone_number'
+  | 'email'
