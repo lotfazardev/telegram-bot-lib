e57a1563b8ff329a23e2b2b27e39e4e99c94848a
Mohammad H. Lotfazar <developer.lotfazar@gmail.com>
2024-12-15 18:37:31 +0330

    docs: add jsdocs for ShippingQuery

--- src/entity/@types/ShippingQuery.ts
+++ src/entity/@types/ShippingQuery.ts
@@ -1,9 +1,28 @@
 import { ShippingAddress } from './ShippingAddress'
 import { User } from './User'
 
+/**
+ * This object contains information about an incoming shipping query.
+ *
+ */
 export interface ShippingQuery {
+  /**
+   * Unique query identifier.
+   */
   id: string
+
+  /**
+   * User who sent the query.
+   */
   from: User
+
+  /**
+   * Bot-specified invoice payload.
+   */
   invoice_payload: string
+
+  /**
+   * User-specified shipping address.
+   */
   shipping_address: ShippingAddress
 }
