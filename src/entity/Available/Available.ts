d3889cf07d62b7ef4d1ca5ea81e5a7761c793e31
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-21 18:21:26 +0330

    feat: implement sendMessage Method in Available Entity

--- src/entity/Available/Available.ts
+++ src/entity/Available/Available.ts
@@ -1,10 +1,20 @@
 import BaseTelegramApiEntity from '@core/BaseTelegramApiEntity'
 import { User } from '../@types/User'
+import { Message } from '@entity/@types/Message'
+import { SendMessageParams } from './@types/SendMessageParams'
 
 export default class Available extends BaseTelegramApiEntity {
   async getMe() {
     return this.apiAgent<User>({
       method: 'getMe',
+    })
+  }
+  async sendMessage(sendMessageParams: SendMessageParams) {
+    return this.apiAgent<Message>({
+      method: 'sendMessage',
+      options: {
+        body: JSON.stringify(sendMessageParams),
+      },
     })
   }
 }
