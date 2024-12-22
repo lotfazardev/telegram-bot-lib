ecefef7031a85752db1cc88acdf1b97a2929a82b
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-12-22 15:05:48 +0330

    feat: add getChat Method to Available Entity with ChatFullInfo Type

--- src/entity/Available/Available.ts
+++ src/entity/Available/Available.ts
@@ -1,12 +1,21 @@
 import BaseTelegramApiEntity from '@core/BaseTelegramApiEntity'
 import { User } from '../@types/User'
 import { Message } from '@entity/@types/Message'
+import { ChatFullInfo } from '../@types/ChatFullInfo'
 import { SendMessageParams } from './@types/SendMessageParams'
 
 export default class Available extends BaseTelegramApiEntity {
   async getMe() {
     return this.apiAgent<User>({
       method: 'getMe',
+    })
+  }
+  async getChat(chat_id: number | string) {
+    return this.apiAgent<ChatFullInfo>({
+      method: 'getChat',
+      options: {
+        body: JSON.stringify({ chat_id }),
+      },
     })
   }
   async sendMessage(sendMessageParams: SendMessageParams) {
