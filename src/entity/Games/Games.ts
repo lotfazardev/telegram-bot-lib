70988b3d860267f89d3550f3c820c63122929204
Mohammad H. Lotfazar <developer.lotfazar@gmail.com>
2024-12-27 21:34:08 +0330

    feat(Games): add sendGame method

--- src/entity/Games/Games.ts
+++ src/entity/Games/Games.ts
@@ -1,3 +1,12 @@
 import BaseTelegramApiEntity from '@core/BaseTelegramApiEntity'
+import { Message } from '@entity/@types/Message'
+import { SendGameParams } from './@types'
 
-export default class Games extends BaseTelegramApiEntity {}
+export default class Games extends BaseTelegramApiEntity {
+  /**
+   * Use this method to send a game. On success, the sent Message is returned.
+   */
+  async sendGame(sendGameParams: SendGameParams) {
+    return this.jsonCall<Message>('sendGame', sendGameParams)
+  }
+}
