cbadf38867f1a7990d9bde0688a67332c79b284e
Mohammad H. Lotfazar <developer.lotfazar@gmail.com>
2025-01-03 15:55:40 +0330

    test: add test for sendInvoice method

---
+++ test/payment.test.ts
@@ -0,0 +1,38 @@
+import Telegram from '@src/Telegram'
+
+describe('💳 Telegram Payment Entity', () => {
+  const apiKey = String(process.env.API_KEY)
+  const userId = Number(process.env.USER_ID)
+  const providerToken = String(process.env.PAYMENT_PROVIDER_TOKEN)
+  const telegramClient = new Telegram({ apiKey })
+
+  it('Validates required environment variables.', () => {
+    expect(apiKey).toBeDefined()
+    expect(apiKey).not.toBe('')
+    expect(providerToken).toBeDefined()
+    expect(providerToken).not.toBe('')
+    expect(userId).toBeDefined()
+    expect(Number.isNaN(userId)).toBe(false)
+  })
+
+  it('Correctly initializes with a valid API key.', () => {
+    expect(telegramClient).toBeDefined()
+  })
+
+  it('Correctly sends invoice to user.', async () => {
+    const result = await telegramClient.sendInvoice({
+      chat_id: 5169431105,
+      title: 'Test Product',
+      description: 'This is a test product description.',
+      payload: 'test_payload',
+      provider_token: providerToken,
+      currency: 'USD',
+      prices: [{ label: 'Test Item', amount: 5000 }],
+    })
+    expect(result).toBeDefined()
+    expect(result.chat.id).toBe(userId)
+    expect(result.invoice).toBeDefined()
+    expect(result.invoice?.title).toBe('Test Product')
+    expect(result.invoice?.description).toBe('This is a test product description.')
+  })
+})
